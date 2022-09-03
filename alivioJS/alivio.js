706c5a1a2e968e241419f956fd53f6a96aac41b5
devbovios <krager.2108@gmail.com>
2022-09-03 23:08:48 +0300

    created an adaptive page

--- alivioJS/alivio.js
+++ alivioJS/alivio.js
@@ -7,3 +7,20 @@ function PopUpShow(){
 function PopUpHide(){
     $("#popup1").hide();
 }
+
+$(document).ready(function(){
+    $("#top_scroll").removeClass("default");
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 80) {
+            $("#top_scroll").addClass("default").fadeIn('fast');
+        } else {
+            $("#top_scroll").removeClass("default").fadeIn('fast');
+        };
+    });
+});
+
+$(document).ready(function () {
+    $(".menu_button").click(function() {
+        $(".menu").toggleClass("show_menu")
+    })
+})
